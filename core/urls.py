46aa5024044224f8dea705087d30e06cff3437d0
Luan Fonseca Farias <luanfonceca@gmail.com>
2012-07-08 22:35:10 -0300

    keep walking

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('core.views',
     url(r'^$', 'public.index', name="index"),
+    url(r'^contact/$', 'public.contact', name="contact"),
 )
--- core/views/public.py
+++ core/views/public.py
@@ -6,8 +6,16 @@ from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.http import HttpResponse
+from core.forms import *
 
 
 def index(request):
     return render_to_response('public/index.html', locals(),
+            context_instance=RequestContext(request))
+            
+def contact(request):
+    contact_form = ContactForm(request.POST or None)
+    if request.method is 'POST':
+        import pdb;pdb.set_trace()
+    return render_to_response('public/contact.html', locals(),
             context_instance=RequestContext(request))
